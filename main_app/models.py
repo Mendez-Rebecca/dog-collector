33b3ba59e10443f7c6981d376834469537a5a9b8
Rebecca <mendez_elizabeth88@hotmail.com>
2023-09-14 16:18:58 -0700

    edit feedings to complete for the day after three feedings

--- main_app/models.py
+++ main_app/models.py
@@ -33,7 +33,7 @@ class Dog(models.Model):
         return reverse('detail', kwargs={'dog_id': self.id})
 
     def fed_for_today(self):
-        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+        return self.feeding_set.filter(date=date.today()).count() == len(MEALS)
 
 class Feeding(models.Model):
     date = models.DateField('Feeding Date')
